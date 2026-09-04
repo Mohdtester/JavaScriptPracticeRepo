06f62e9e20f58cb0e6dcddd25c2681fe60817117
Mohdtester <109057007+Mohdtester@users.noreply.github.com>
2026-09-04 22:13:42 +0530

    Twofiles uploaded

---
+++ demo-two.js
@@ -0,0 +1,7 @@
+process.stdout.write("A");
+process.stdout.write("B");
+process.stdout.write("C");
+console.log("Mohammed Ali");
+console.log('Mohammed Ali')
+console.log(9);
+console.log(9,"mohammed",39,"indian")
